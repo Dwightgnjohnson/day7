c54b7fb8238f8dbd5d9e13c1b2611b18be714b73
dwightgnjohnson <dwightgnjohnson@gmail.com>
2014-09-30 14:26:46 -0500

    almost done with inheritance homework

--- homework_inheritance.rb
+++ homework_inheritance.rb
@@ -1,13 +1,27 @@
-class Fish
+# Create a set of classes using inheritance
+# It should show that Pandas and Sharks both eat Fish
+# When a Panda or a shark eats, it should log this out to a Log
+
+
+class Animal
+
   def eat
-
+    p "I, The #{self.class}, eat fish!"
   end
-end
-
-class Panda < Fish
 
 end
 
-class Shark < Fish
+class Panda < Animal
 
 end
+
+class Shark < Animal
+
+end
+
+
+shark1 = Shark.new
+shark1.eat
+
+panda1000 = Panda.new
+panda1000.eat
